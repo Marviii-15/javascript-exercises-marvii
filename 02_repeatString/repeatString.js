8a07b827a8d8bc5b25051898dd4513f19961b44e
Marviii-15 <marviluzpaternina@gmail.com>
2023-11-11 17:50:06 -0400

    m

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(cadena , n) {
+let result = "";
+ for (let i = 0 ; i < n; i++) {
+    result += cadena;
+ }
+ if (n < 0){
+    result = 'ERROR' ;
+ }
+ return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
